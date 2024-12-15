413b1eb4589150fc9aebfc2de3a0aeb5a9fef20e
Ondřej Valach <valach.ondra@seznam.cz>
2024-12-15 15:49:30 +0300

    Not loading models iteratively, but once before the inference

--- BackendRunner.py
+++ BackendRunner.py
@@ -1,4 +1,3 @@
-# Import necessary modules and functions for pose prediction, image processing, and machine learning
 from assets.predict_pose import predict_pose, create_mediapipe_models
 from assets.pose_utils import crop_pad_image
 from assets.crop_frames import crop_frame
@@ -16,6 +15,9 @@ class BackendRunner:
         self.checkpoint_pose = checkpoint_pose
         self.checkpoint_mae = checkpoint_mae
         self.checkpoint_dino = checkpoint_dino
+        self.models_pose = None
+        self.model_mae = None
+        self.model_dino_hand = None
         # Set the device to GPU if available, otherwise use CPU
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -43,48 +45,49 @@ class BackendRunner:
 
         return np.array(frames)
 
+    def load_models(self):
+        self.models_pose = create_mediapipe_models(self.checkpoint_pose)
+        arch = 'vit_base_patch16'
+        self.model_mae = create_mae_model(arch, self.checkpoint_mae)
+        self.model_dino_hand = create_dino_model(self.checkpoint_dino)
+
     def pose_video(self, video_dir):
         # Predict pose for a video
-        models = create_mediapipe_models(self.checkpoint_pose)
         video = self.load_video_frames(video_dir)
-        prediction = predict_pose(video, models, 4)
+        prediction = predict_pose(video, self.models_pose, 4)
         return prediction
 
     def pose_img(self, image_dir):
         # Predict pose for a single image
-        models = create_mediapipe_models(self.checkpoint_pose)
         image = cv2.imread(image_dir)
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        video = [rgb_image]
-        prediction = predict_pose(video, models, 4)
+        image = [rgb_image]
+        prediction = predict_pose(image, self.models_pose, 4)
         return prediction
 
     def mae(self, images):
         # Generate MAE embeddings for a list of images
-        arch = 'vit_base_patch16'
-        model = create_mae_model(arch, self.checkpoint_mae)
-        model = model.to(self.device)
+        self.model_mae = self.model_mae.to(self.device)
 
         mae_embeddings = []
         for image in images:
-            mae_embedding = mae_predict(image, model, transform_mae, self.device)
+            mae_embedding = mae_predict(image, self.model_mae, transform_mae, self.device)
             mae_embeddings.append(mae_embedding)
         return mae_embeddings
 
     def dino(self, pose_output, lhand=0):
         # Generate DINO embeddings for pose output
-        hand_model = create_dino_model(self.checkpoint_dino)
-        hand_model.to(self.device)
+        self.model_dino_hand.to(self.device)
 
         rhand_embeddings = []
         for i in range(len(pose_output["images"])):
-            right_features = dino_predict(pose_output["cropped_right_hand"][i], hand_model, transform_dino, self.device)
+            right_features = dino_predict(pose_output["cropped_right_hand"][i], self.model_dino_hand, transform_dino, self.device)
             rhand_embeddings.append(right_features)
 
         if lhand:
             lhand_embeddings = []
             for i in range(len(pose_output["images"])):
-                left_features = dino_predict(pose_output["cropped_left_hand"][i], hand_model, transform_dino, self.device)
+                left_features = dino_predict(pose_output["cropped_left_hand"][i], self.model_dino_hand, transform_dino, self.device)
                 lhand_embeddings.append(left_features)
             features = np.hstack((lhand_embeddings, rhand_embeddings))
             return np.squeeze(features)
@@ -97,7 +100,7 @@ class BackendRunner:
         return sim
 
 if __name__ == "__main__":
-    # Define paths to checkpoints and image directory
+    # Define paths to checkpoints and image input
     checkpoints_pose = "checkpoints/pose"
     checkpoint_mae = "checkpoints/mae/16-07_21-52-12/checkpoint-440.pth"
     checkpoint_dino = "checkpoints/dino/hand/teacher_checkpoint.pth"
@@ -105,12 +108,14 @@ if __name__ == "__main__":
 
     # Create an instance of BackendRunner
     runner = BackendRunner(checkpoints_pose, checkpoint_mae, checkpoint_dino)
-    # Perform pose prediction on an image
+    # Load models
+    runner.load_models()
+    # Perform pose prediction on an image and crop
     pose_output = runner.pose_img(image_dir)
     # Generate MAE embeddings from cropped images
     mae_embeddings = runner.mae(pose_output["cropped_images"])
     # Generate DINO embeddings from pose output
     dino_embeddings = runner.dino(pose_output, 0)
-
-    # Calculate similarity between DINO embeddings
+    
+    # Calculate similarity between embeddings
     sim = runner.similarity(dino_embeddings, dino_embeddings)
