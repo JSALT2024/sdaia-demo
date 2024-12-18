83e41e86e4e89beceb1658f639838d912f7db416
Václav Javorek <javorek.v@gmail.com>
2024-12-18 11:26:03 +0300

    Revert "Merge pull request #2 from JSALT2024/devel"
    This reverts commit 816846143bb8a2b265e43df4fa06d86d906ab0ce, reversing
    changes made to 9a798c963b6e9f3ef4b026a621dc24852321994e.

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ print("Models loaded.")
 def process_image(input_image, source):
     print("Processing image...")
     if source == None:
-        return f"<div style='font-size: 300px; text-align: center;'>Error</div>", None, None
+        return f"<div style='font-size: 300px; text-align: center;'>Error</div>"
     else:
-        prediction, detected_hand, matched_hand = handler.predict(input_image, source)
+        prediction = handler.predict(input_image, source)
         print("Prediction done.")
-        return f"<div style='font-size: 300px; text-align: center;'>{prediction}</div>", detected_hand, matched_hand
+        return f"<div style='font-size: 300px; text-align: center;'>{prediction}</div>"
 
 def process_input(input_image):
     if input_image == None:
@@ -36,11 +36,7 @@ iface = gr.Interface(
     inputs=[
         gr.Image(type="filepath", sources=["upload", "webcam"], label="Upload an image or take a picture"),
     ],
-    outputs=[
-        gr.HTML(label="Predicted Sign"),
-        gr.Image(label="Detected Hand", type="numpy"),
-        gr.Image(label="Most Similar Database Match", type="numpy")
-    ],
+    outputs=gr.HTML(label="Predicted Sign"), 
     title="Sign Language Recognition - Alphanumerals in SAUDI SL",  # Title of the interface
     description="This demo is a proof of concept for the recognition system of the SAUDI SL. It uses MediaPipe and DINO in the backend, trained on the SAUDI SL dataset from Mohammad Alghannami and Maram Aljuaid.\n\n" + 
     "You can try signing numerals 1-9 with your right hand in Saudi Sign language."
