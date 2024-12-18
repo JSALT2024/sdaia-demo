1f924df8721e4f29be1c17c8c6dcb54a75c4ca88
Václav Javorek <javorek.v@gmail.com>
2024-12-18 11:07:49 +0300

    detected hand + matched hand boxes

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ print("Models loaded.")
 def process_image(input_image, source):
     print("Processing image...")
     if source == None:
-        return f"<div style='font-size: 300px; text-align: center;'>Error</div>"
+        return f"<div style='font-size: 300px; text-align: center;'>Error</div>", None, None
     else:
-        prediction = handler.predict(input_image, source)
+        prediction, detected_hand, matched_hand = handler.predict(input_image, source)
         print("Prediction done.")
-        return f"<div style='font-size: 300px; text-align: center;'>{prediction}</div>"
+        return f"<div style='font-size: 300px; text-align: center;'>{prediction}</div>", detected_hand, matched_hand
 
 def process_input(input_image):
     if input_image == None:
@@ -36,7 +36,11 @@ iface = gr.Interface(
     inputs=[
         gr.Image(type="filepath", sources=["upload", "webcam"], label="Upload an image or take a picture"),
     ],
-    outputs=gr.HTML(label="Predicted Sign"), 
+    outputs=[
+        gr.HTML(label="Predicted Sign"),
+        gr.Image(label="Detected Hand", type="numpy"),
+        gr.Image(label="Most Similar Database Match", type="numpy")
+    ],
     title="Sign Language Recognition - Alphanumerals in SAUDI SL",  # Title of the interface
     description="This demo is a proof of concept for the recognition system of the SAUDI SL. It uses MediaPipe and DINO in the backend, trained on the SAUDI SL dataset from Mohammad Alghannami and Maram Aljuaid.\n\n" + 
     "You can try signing numerals 1-9 with your right hand in Saudi Sign language."
